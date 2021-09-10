4970266b0415849c3f312e699c965cefa793f0a5
marcosciaxx <marcosciaxx@gmail.com>
2021-09-11 02:00:42 +0500

    Pieces can Check where to move now (More Advanced)

--- src/Checkers/Controller/Move.java
+++ src/Checkers/Controller/Move.java
@@ -3,23 +3,40 @@ package Checkers.Controller;
 import Checkers.Models.BoardInfo;
 import Checkers.Models.Piece;
 import Checkers.Models.Spot;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class Move {
+public class Move implements Initializable {
 
-     public void movePiece(Piece piece){
+    ArrayList<Spot> killableMoves = new ArrayList<>();
+
+    int checkRow;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        killableMoves = new ArrayList<>();
+    }
+
+    public void movePiece(Piece piece){
 
          piece.setOnMousePressed(MouseEvent -> {
 
              System.out.println(piece.getSpot().getRow_number()+ " " + piece.getSpot().getColumn_number());
-             ArrayList<Spot> availableSpotsL = getSingleMoves(piece.getSpot());
+             ArrayList<Spot> singleAvailableMoves = getSingleMoves(piece.getSpot());
+             getKillableMovesInRight(piece.getSpot(),true);
+             getKillableMovesInLeft(piece.getSpot(),true);
 //             ArrayList<Spot> availableSpotsR = getMovableSpotsRight(piece.getSpot());
 
-             for(Spot spot : availableSpotsL)
+
+             for(Spot spot : singleAvailableMoves)
+                 spot.setFill(Color.RED);
+             for(Spot spot : killableMoves)
                  spot.setFill(Color.RED);
 //             BoardController.spots[2][4].setFill(Color.RED);
 //             for(Spot spot : availableSpotsR)
@@ -42,7 +59,7 @@ public class Move {
 
          int checkRow = spot.getPiece().getPlayerAssociated().equals(BoardController.playerOne) ? -1 : 1;
 
-         if (row_number == 0)
+         if (row_number == 0 && checkRow==-1)
              return availableSpots;
 
          Spot spotToBeAdded=null;
@@ -68,6 +85,65 @@ public class Move {
              availableSpots.add(spotToBeAdded);
 
          return availableSpots;
+     }
+
+     public void getKillableMovesInRight(Spot spot,boolean firstTime){
+         int row_number = spot.getRow_number();
+         int col_number = spot.getColumn_number();
+
+         if (firstTime)
+             checkRow = spot.getPiece().getPlayerAssociated().equals(BoardController.playerOne) ? -1 : 1;
+
+         if(spot.getRow_number()==0 && checkRow==-1)
+             return;
+         if(spot.getRow_number()==7 && checkRow==1)
+             return;
+
+         Spot spotToBeAdded = getRequiredSpot(row_number,col_number,checkRow,1);
+
+         if(!checkEmpty(spotToBeAdded)){
+             spotToBeAdded = getRequiredSpot(row_number+checkRow , col_number+1, checkRow,1);
+             if(checkEmpty(spotToBeAdded)) {
+                 killableMoves.add(spotToBeAdded);
+                 getKillableMovesInRight(spotToBeAdded,false);
+             }
+         }
+     }
+
+    public void getKillableMovesInLeft(Spot spot,boolean firstTime){
+        int row_number = spot.getRow_number();
+        int col_number = spot.getColumn_number();
+
+        if (firstTime)
+            checkRow = spot.getPiece().getPlayerAssociated().equals(BoardController.playerOne) ? -1 : 1;
+
+        if(spot.getRow_number()==0 && checkRow==-1)
+            return;
+        if(spot.getRow_number()==7 && checkRow==1)
+            return;
+
+        Spot spotToBeAdded = getRequiredSpot(row_number,col_number,checkRow,-1);
+
+        if(!checkEmpty(spotToBeAdded)){
+            spotToBeAdded = getRequiredSpot(row_number+checkRow , col_number-1, checkRow,-1);
+            if(checkEmpty(spotToBeAdded)) {
+                killableMoves.add(spotToBeAdded);
+                getKillableMovesInLeft(spotToBeAdded,false);
+            }
+        }
+    }
+
+     public Spot getRequiredSpot(int row_number, int col_number, int checkRow , int col_direction){
+         Spot spotToBeAdded=null;
+
+         for(int i=0 ; i < 64 ;i++){
+             if(BoardController.spots[i].getRow_number()==row_number+checkRow && BoardController.spots[i].getColumn_number()==col_number+col_direction) {
+                 spotToBeAdded = BoardController.spots[i];
+                 break;
+             }
+         }
+
+         return spotToBeAdded;
      }
 
      public ArrayList<Spot> getMovableSpotsRight(Spot spot){
@@ -110,5 +186,6 @@ public class Move {
          return spot.getPiece() == null;
 //         return true;
      }
+
 
 }
