9f53f16d379dbdc85c7cae5307eaa314c59a3b36
marcosciaxx <marcosciaxx@gmail.com>
2021-09-12 21:04:58 +0500

    Created Methods to check win and lose situations

--- src/Checkers/Controller/CheckWin.java
+++ src/Checkers/Controller/CheckWin.java
@@ -1,4 +1,27 @@
 package Checkers.Controller;
 
+import Checkers.Models.Piece;
+import Checkers.Models.Spot;
+import javafx.scene.layout.Pane;
+
 public class CheckWin {
+
+    private static int remaining_pieces;
+    private static Piece remaining_piece;
+
+    public static int checkWinner(){
+        for(Spot spot : BoardController.spots){
+            if(spot.getPiece()!=null){
+                remaining_pieces++;
+                remaining_piece=spot.getPiece();
+            }
+        }
+
+        if(remaining_pieces==1) {
+            if (remaining_piece.getPlayerAssociated().equals(BoardController.playerOne)) return 1;
+            else if (remaining_piece.getPlayerAssociated().equals(BoardController.playerTwo)) return 2;
+        }
+
+        return -1;
+    }
 }
